37ca845ba48ad437423873a8751ccbab20abce76
bigandy5climate <snolly@protonmail.com>
2022-07-27 14:34:39 +0200

    rr

--- SmartContracts/ICO/migrations/2_deploy_contracts.js
+++ SmartContracts/ICO/migrations/2_deploy_contracts.js
@@ -1,6 +1,16 @@
-const GustavoCoinCrowdsale = artifacts.require('./GustavoCoinCrowdsale.sol');
-const GustavoCoin = artifacts.require('./GustavoCoin.sol');
+const ICO = artifacts.require('./ICO.sol');
+const KM = artifacts.require('./KM.sol');
+const Web3=require('web3');
+ function initWeb3() {
+  
+      
+      web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    
+    web3 = new Web3(web3Provider);
 
+    return web3;
+  }
+const web3 = initWeb3();
 module.exports = function(deployer, network, accounts) {
     const openingTime = web3.eth.getBlock('latest').timestamp + 2; // two secs in the future
     const closingTime = openingTime + 86400 * 20; // 20 days
@@ -9,16 +19,16 @@ module.exports = function(deployer, network, accounts) {
 
     return deployer
         .then(() => {
-            return deployer.deploy(GustavoCoin);
+            return deployer.deploy(KM);
         })
         .then(() => {
             return deployer.deploy(
-                GustavoCoinCrowdsale,
+                ICO,
                 openingTime,
                 closingTime,
                 rate,
                 wallet,
-                GustavoCoin.address
+                KM.address
             );
         });
 };
