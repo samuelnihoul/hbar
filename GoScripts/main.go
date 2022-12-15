63fc0b44bd61d8eb531e6327c0ae31c798ec3b5f
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2022-12-15 12:52:07 +0100

    nothing itm

--- GoScripts/main.go
+++ GoScripts/main.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/hashgraph/hedera-sdk-go/v2"
-)
-
 func main() {
-	// generates a new private key
-	privateKey, err := hedera.PrivateKeyGenerateEd25519()
-	if err != nil {
-		fmt.Println(err)
-	}
-	// prints the generated private key
-	fmt.Printf("the private key is %v", privateKey)
+	ToEVMAddress()
 }
--- SmartContracts/Tests/MintAndTransfer.sol
+++ SmartContracts/Tests/MintAndTransfer.sol
@@ -1,18 +1,23 @@
 //SPDX-Identifier: MIT
 pragma solidity 0.8.17;
-import '../Auction2/hts-precompile/HederaTokenService.sol';
-import '../Auction2/hts-precompile/TokenCreateContract.sol';
+import "../Auction2/hts-precompile/HederaTokenService.sol";
+import "../Auction2/hts-precompile/TokenCreateContract.sol";
 
-contract MintTransfer is HederaTokenService{
-    address public  tokenAddress;
-    address public  payer;
+contract MintTransfer is HederaTokenService {
+    address public tokenAddress;
+    address public payer;
     TokenCreateContract public tokenCreateContract;
-    HederaTokenService public hederaTokenService;
-function inti (address _payer)external payable{
-    tokenCreateContract= new TokenCreateContract();
-    payer=_payer;
-    tokenAddress=tokenCreateContract.createFungible('AAAAC3NzaC1lZDI1NTE5AAAAIOpUhraATD0hPqcdbJ94CCneiJob9piAawt6fPYgRqg1',"AAAAC3NzaC1lZDI1NTE5AAAAIOpUhraATD0hPqcdbJ94CCneiJob9piAawt6fPYgRqg1" ,msg.sender,11111111);
-mintToken(tokenAddress,1,new bytes[](0));
-transferToken(tokenAddress,address(this),payer,1);
-}
+
+    function inti() external payable {
+        tokenCreateContract = new TokenCreateContract();
+        payer = address(this);
+        tokenAddress = tokenCreateContract.createFungible(
+            "AAAAC3NzaC1lZDI1NTE5AAAAIOpUhraATD0hPqcdbJ94CCneiJob9piAawt6fPYgRqg1",
+            "AAAAC3NzaC1lZDI1NTE5AAAAIOpUhraATD0hPqcdbJ94CCneiJob9piAawt6fPYgRqg1",
+            payer,
+            11111111
+        );
+        mintToken(tokenAddress, 1, new bytes[](0));
+        transferToken(tokenAddress, address(this), msg.sender, 1);
+    }
 }
