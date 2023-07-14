fdbe86ce55435ba6a889082e706fb274ce25a04e
Bigandy4climate <samuelnihoul@hotmail.fr>
2023-07-14 14:25:42 +0200

    corrrect metadata

--- NFTMetadata/metadataGenerator.py
+++ NFTMetadata/metadataGenerator.py
@@ -2,9 +2,9 @@ import pandas as pd
 df = pd.read_csv('metadata.csv')
 for i in range(0, 8):
     for j in range(0, 5):
-        with open(f'metadata/{i}.{j}.json', 'w') as f:
+        with open(f'metadata/{i}.{(j+1)%5}.json', 'w') as f:
             id = i*5+j
             name = df['imagename'][id]
             cid = df['cid'][id]
             f.write(
-                f"{{ \"name\":\"{i}.{j}\".\"image\":\"https://ipfs.io/ipfs/{cid}\"}}")
+                f"{{ \"name\":\"{i}.{(j+1)%5}\".\"image\":\"https://ipfs.io/ipfs/{cid}\"}}")
