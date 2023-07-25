3b9e10a885d73ab61e2301c877f51dcf06a367d2
Bigandy4climate <samuelnihoul@hotmail.fr>
2023-07-25 09:45:28 +0200

    all the json files

--- nodeCLI/src/index.js
+++ nodeCLI/src/index.js
@@ -1,5 +1,17 @@
 import * as fs from "fs";
+import b from './NFTs.json' assert {type: 'json'}
 function a() {
-  gile = fs.rea;
+  console.log(b)
+  for (let i = 0; i < 40; i++) {
+    fs.writeFileSync(
+      `/home/nuhutuh25/hbar/nodeCLI/src/metadata/${b[i]['name']}.json`, JSON.stringify(
+        {
+          name: b[i]['name']
+          ,
+          image: `https://ipfs.io/ipfs/${b[i]['image']}?filename=${b[i]['name']}`
+        }
+      )
+    )
+  }
 }
 a();
