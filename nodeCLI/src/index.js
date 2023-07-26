30bb2f4d314e09544cd1338b9da7c247b1c98a03
Bigandy4climate <samuelnihoul@hotmail.fr>
2023-07-26 17:18:26 +0200

    idk

--- nodeCLI/src/index.js
+++ nodeCLI/src/index.js
@@ -4,37 +4,11 @@ import { identifyService } from 'libp2p/identify'
 import { noise } from '@chainsafe/libp2p-noise'
 import { yamux } from '@chainsafe/libp2p-yamux'
 import { webSockets } from '@libp2p/websockets'
+import { tcp } from '@libp2p/tcp'
 import { bootstrap } from '@libp2p/bootstrap'
 import { MemoryDatastore } from 'datastore-core'
 import b from './NFTs.json' assert {type: 'json'}
-const datastore = new MemoryDatastore()
 import { MemoryBlockstore } from 'blockstore-core'
-const blockstore = new MemoryBlockstore()
-const libp2p = await createLibp2p({
-  datastore,
-  transports: [
-    webSockets()
-  ],
-  connectionEncryption: [
-    noise()
-  ],
-  streamMuxers: [
-    yamux()
-  ],
-  peerDiscovery: [
-    bootstrap({
-      list: [
-        "/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
-        "/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
-        "/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
-        "/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt"
-      ]
-    })
-  ],
-  services: {
-    identify: identifyService()
-  }
-})
 function a() {
   console.log(b)
   for (let i = 0; i < 40; i++) {
@@ -48,11 +22,55 @@ function a() {
       )
     )
   }
+} async function createNode() {
+  // the blockstore is where we store the blocks that make up files
+  const blockstore = new MemoryBlockstore()
+
+  // application-specific data lives in the datastore
+  const datastore = new MemoryDatastore()
+
+  // libp2p is the networking layer that underpins Helia
+  const libp2p = await createLibp2p({
+    datastore,
+    addresses: {
+      listen: [
+        '/ip4/127.0.0.1/tcp/0'
+      ]
+    },
+    transports: [
+      tcp()
+    ],
+    connectionEncryption: [
+      noise()
+    ],
+    streamMuxers: [
+      yamux()
+    ],
+    peerDiscovery: [
+      bootstrap({
+        list: [
+          '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
+          '/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa',
+          '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+          '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt'
+        ]
+      })
+    ],
+    services: {
+      identify: identifyService()
+    }
+  })
+
+  return await createHelia({
+    datastore,
+    blockstore,
+    libp2p
+  })
 }
 import { createHelia } from 'helia'
 import { json } from '@helia/json'
 
-const helia = await createHelia()
+const helia = await createNode()
 const j = json(helia)
 
 
